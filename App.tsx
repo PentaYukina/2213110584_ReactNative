451d928558d90784fd7a825c0a62858150a06800
PentaYukina <jarahaama@gmail.com>
2024-08-03 11:30:32 +0700

    Practice1_useEffect

--- App.tsx
+++ App.tsx
@@ -1,10 +1,11 @@
 import { StatusBar } from "expo-status-bar";
 import React, { useEffect, useState } from "react";
-import { StyleSheet, TextInput, View } from "react-native";
+import { StyleSheet, TextInput, View, Alert } from "react-native";
 import AppFooter from "./components/AppFooter";
 import AppHeader from "./components/AppHeader";
 import Content from "./components/Content";
 import { stylesPractice } from "./styles/styles";
+
 
 export default function App(): React.JSX.Element {
   const [fullname, setFullname] = useState("");
@@ -20,10 +21,15 @@ export default function App(): React.JSX.Element {
     console.log(`Fullname has changed to : ${fullname}`);
   }, [fullname]); //This will run whenever `fullname' changes
 
+  const handleButtonClick =()=>{
+    Alert.alert("Hello", `Input your fullname : ${fullname}`);
+  };
+
   return (
     <View style={stylesPractice.container}>
       <AppHeader fullname={fullname} message={message} />
-      <Content message={message} fullname={fullname} />
+      {/* <Content message={message} fullname={fullname} /> */}
+      <Content message={message} onButtonClick = {handleButtonClick}/>
       <AppFooter footerMessage={FooterMessage} />
       <TextInput
         style={stylesPractice.input}
--- components/Content.tsx
+++ components/Content.tsx
@@ -4,21 +4,22 @@ import { stylesPractice } from "../styles/styles";
 
 type Content = {
   message: string;
-  fullname: string;
+  //fullname: string;
+  onButtonClick:() => void;
 };
 
-const Content = ({ message, fullname }: Content): React.JSX.Element => {
-  const [displayFullname, setDisplayFullname] = React.useState('');
-
+const Content = ({ message, onButtonClick }: Content): React.JSX.Element => {
+  /* const [displayFullname, setDisplayFullname] = React.useState('');
+  
   const handleButtonClick = () => {
     setDisplayFullname(fullname);
     Alert.alert("Hello", `Input your fullname : ${fullname}`);
-  };
+  }; */
   return (
     <View style={stylesPractice.content}>
       <Text style={stylesPractice.text}>{message}</Text>
-      <Text style={stylesPractice.text}>{displayFullname}</Text>
-      <Button title="Click Me" onPress={handleButtonClick} color="blue" />
+      {/* <Text style={stylesPractice.text}>{displayFullname}</Text> */}
+      <Button title="Click Me" onPress={onButtonClick} color="#029cb0" />
     </View>
   );
 };
