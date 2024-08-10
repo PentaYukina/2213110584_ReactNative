e3795a2010dbab16bc2a1e2cfbc4a599d5d5735a
PentaYukina <jarahaama@gmail.com>
2024-08-10 11:12:48 +0700

    Practice4_FlatList Component(Example)

--- App.tsx
+++ App.tsx
@@ -1,16 +1,19 @@
 import { View, Text } from 'react-native'
 import React from 'react'
-import ProfileScreen from './components/ProfileScreen'
-import UseEffectEx from './components/UseEffectEx'
+import FlastListExample from './components/FlastListExample'
+import FlatListcallBackend from './components/FlatListcallBackend'
 
 
 const App = ():React.JSX.Element => {
   
   return (
     <View>
-      <ProfileScreen/> 
+      {/* <ProfileScreen/>  */}
+      {/* <FlastListExample/> */}
+      <FlatListcallBackend/>
     </View>
   )
 }
 
 export default App
+
