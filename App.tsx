a0e94f8033fe0fc4b46aa4362fb8832f187303be
PentaYukina <jarahaama@gmail.com>
2024-09-07 11:21:31 +0700

    Lecture9_Navigation

--- App.tsx
+++ App.tsx
@@ -1,45 +1,91 @@
+// Only import react-native-gesture-handler on native platforms
+import "react-native-gesture-handler";
+
 import { View, Text } from "react-native";
 import React from "react";
+import { HeaderButtonsProvider } from "react-navigation-header-buttons";
+import { SafeAreaProvider } from 'react-native-safe-area-context'
 
 import HomeScreen from "./screens/HomeScreen";
 import AboutScreen from "./screens/AboutScreen";
 import CreaePostScreen from "./screens/CreaePostScreen";
+import MenuScreen from "./screens/MenuScreen";
+import ProductScreen from "./screens/ProductScreen";
 
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createDrawerNavigator } from "@react-navigation/drawer";
 
-const App = (): React.JSX.Element => {
-  const HomeStack = createNativeStackNavigator();
+const HomeStack = createNativeStackNavigator();
+const ProductStack = createNativeStackNavigator();
 
+const Drawer = createDrawerNavigator();
+
+function HomeStackScreen() {
   return (
-    <NavigationContainer>
-      <HomeStack.Navigator
-        initialRouteName="Home"
-        screenOptions={{//Global
-          headerStyle: { backgroundColor: "#F7B5CA" },
-          headerTintColor: "white",
-          headerTitleStyle: { fontWeight: "bold" },
-          headerTitleAlign: "center",
-        }}
-      >
-        <HomeStack.Screen
-          name="Home"
-          component={HomeScreen}
-          options={{ title: "หน้าหลัก" }}
-        />
-        <HomeStack.Screen
-          name="About"
-          component={AboutScreen}
-          /* options={{ title: "เกี่ยวกับเรา" ,
+    <HomeStack.Navigator
+      initialRouteName="Home"
+      screenOptions={{
+        //Global
+        // headerStyle: { backgroundColor: "#F7B5CA" },
+        // headerTintColor: "white",
+        headerTitleStyle: { fontWeight: "bold" },
+        // headerTitleAlign: "center",
+      }}
+    >
+      <HomeStack.Screen
+        name="Home"
+        component={HomeScreen}
+        // options={{ title: "หน้าหลัก" }}
+      />
+      <HomeStack.Screen
+        name="About"
+        component={AboutScreen}
+        /* options={{ title: "เกี่ยวกับเรา" ,
             headerStyle:{backgroundColor:'#FFD0D0'},
             headerTintColor:'white',
             headerTitleStyle:{fontWeight:'bold'},
             headerTitleAlign:'center'
           }} */
-        />
-        <HomeStack.Screen name="CreatePost" component={CreaePostScreen} />
-      </HomeStack.Navigator>
-    </NavigationContainer>
+      />
+      {/* <HomeStack.Screen name="CreatePost" component={CreaePostScreen} /> */}
+    </HomeStack.Navigator>
+  );
+}
+
+function ProductStackScreen() {
+  return (
+    <ProductStack.Navigator
+      initialRouteName="Home"
+      screenOptions={{
+        //Global
+        headerTitleStyle: { fontWeight: "bold" },
+      }}
+    >
+      <ProductStack.Screen
+        name="Home"
+        component={ProductScreen}
+      />
+    </ProductStack.Navigator>
+  );
+}
+
+const App = (): React.JSX.Element => {
+  return (
+    <SafeAreaProvider>
+    <HeaderButtonsProvider stackType="native">
+      <NavigationContainer>
+        <Drawer.Navigator 
+        screenOptions={{ headerShown: false }}
+        drawerContent={props => <MenuScreen{...props}/>}
+        >
+          <Drawer.Screen name="HomeStack" component={HomeStackScreen} />
+          <Drawer.Screen name="ProductStack" component={ProductStackScreen} />
+        </Drawer.Navigator>
+          
+      </NavigationContainer>
+    </HeaderButtonsProvider>
+    </SafeAreaProvider>
   );
 };
 
